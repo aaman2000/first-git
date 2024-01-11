6bc29a4a712bc2d18ee4ae5ad6dea88a558e05ab
Sweta Kumari <swetahzb12334@gmail.com>
2024-01-11 20:50:20 +0530

    CalciDriver class

--- Calci.java
+++ Calci.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 interface Calci{
- public int getSum(int a,int b);
- public int getProduct(int a,int b);
+ public int getSub(int a,int b);
+ public int getdiv(int a,int b);
 }
 class Calciimp implements Calci{
   public int getSub(int a,int b){
@@ -10,4 +12,16 @@ class Calciimp implements Calci{
     return(a/b);
 
 }
+}
+class CalciDriver{
+  public static void main(String[] args) {
+    Calciimp c=new Calciimp();
+    Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number:");
+   int n1=sc.nextInt();
+   System.out.println("enter 2nd number:");
+   int n2=sc.nextInt();
+   System.out.println(n1+"-"+n2+"="+c.getSub(n1, n2));
+   System.out.println(n1+"/"+n2+"="+c.getdiv(n1, n2));
+  }
 }
